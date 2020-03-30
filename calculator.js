984eeebc9248e3b4a8448bd25a989e948bb2fea7
WidadSucitra <62865740+WidadSucitra@users.noreply.github.com>
2020-03-30 13:44:02 +0800

    Membuat source code

---
+++ calculator.js
@@ -0,0 +1,11 @@
+console.log ("this is Calculator Kecocokan")
+var nama1 = prompt ("Masukkan nama pertama")
+var nama2 = prompt("Masukkan nama kedua")
+
+console.log((nama1) + " and " + (nama2));
+kecocokan = Math.round(Math.random()*100)
+if (kecocokan >= 50){
+    console.log(kecocokan + "% Kalian Berjodoh")
+}else if (kecocokan < 50){
+    console.log(kecocokan + "% Kalian Tidak Berjodoh")
+}
